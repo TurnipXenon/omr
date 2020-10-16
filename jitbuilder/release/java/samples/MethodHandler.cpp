12e6c339b82fcada9189040defc062feccb71f1e
Allan Manuba <manuba@ualberta.ca>
2020-10-16 16:31:15 -0600

    WIP todo

--- jitbuilder/release/java/samples/MethodHandler.cpp
+++ jitbuilder/release/java/samples/MethodHandler.cpp
@@ -2,6 +2,7 @@
 
 JNIEXPORT jint JNICALL Java_MethodHandler_invoke
   (JNIEnv * env, jobject thisObject, jint oldValue) {
+      // todo: Pass it to the Jit builder library
       ++oldValue;
       return oldValue;
   }
