0e97c3c6264ffb058bb800962d2d6e044f30f2f5
Allan Manuba <manuba@ualberta.ca>
2020-10-21 21:36:04 -0600

    WIP Create make file version

--- jitbuilder/release/java/samples/jitbuilder_release_java_samples_Simple.cpp
+++ jitbuilder/release/java/samples/jitbuilder_release_java_samples_Simple.cpp
@@ -1,7 +1,8 @@
 #include "jitbuilder_release_java_samples_Simple.h"
+
 #include <iostream>
 
 JNIEXPORT void JNICALL Java_jitbuilder_release_java_samples_Simple_test
   (JNIEnv *, jclass) {
-      std::cout << "Main working\n";
+    std::cout << "Hello JNI\n";
   }
